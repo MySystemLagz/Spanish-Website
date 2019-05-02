a474f9f85de62f9fcb845a183d8082ccb79bcfe8
MySystemLagz <43154408+MySystemLagz@users.noreply.github.com>
2019-05-02 10:38:31 -0500

    Update language.js

--- language.js
+++ language.js
@@ -4,7 +4,7 @@ function ChangeText() {
 
     const english = ['<i class="fas fa-globe"></i> Toggle Language', 'Solar', 'Convert sunlight into clean energy with solar panels on your existing roof, or a brand new Solar Roof.', 'A home battery designed to store your clean energy, so you can use it anytime you want—at night or during an outage.', 'Energy'];
 
-    const spanish = ['<i class="fas fa-globe"></i> Cambiar Idioma', 'Solar', 'Convierta la luz solar en energía limpia con paneles solares en su tejado existente, o un nuevo techo solar.', 'Una hogar batería diseñada para almacenar tu energía limpia, para que puedas usarla en cualquier momento que quieras, por la noche o durante una interrupción.', 'Energía'];
+    const spanish = ['<i class="fas fa-globe"></i> Cambiar Idioma', 'Solar', 'Convierta la luz solar en energía limpia con paneles solares en su tejado existente, o un nuevo techo solar.', 'Una batería de hogar diseñada para almacenar tu energía limpia, para que puedas usarla en cualquier momento que quieras, por la noche o durante una interrupción.', 'Energía'];
 
     if (language === 0) {
         for (let i = 0; i < spanish.length; i++) {
